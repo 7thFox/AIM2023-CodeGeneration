9f9a5f7522739793e5c10c67c9acd30668ed4adb
Josh Bergman <joshbe@skyward.com>
2023-07-05 11:22:18 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic.FileIO;
+
+namespace AIM2023_CodeGeneration
+{
+    internal class Program
+    {
+
+        /// Goal: Input CSV(s), Output classes with static values
+        static void Main(string[] args)
+        {
+            var folder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            GenerateFile(folder, "StateDisabilityWI.csv");
+            GenerateFile(folder, "StateDualEnrollmentTypeWI.csv");
+            GenerateFile(folder, "StateEducationEnvironmentWI.csv");
+
+            Process.Start(folder);// open folder in windows explorer
+        }
+
+        static void GenerateFile(string folder, string fileName)
+        {
+            var fileNameNoExt = Path.GetFileNameWithoutExtension(fileName);
+            var inputFilePath = Path.Combine(folder, fileName);
+            var outputFilePath = Path.Combine(folder, fileNameNoExt + ".cs");
+
+            // Note: Our input could be from anywhere, UI, API calls, etc
+            using (TextFieldParser parser = new TextFieldParser(inputFilePath))
+            {
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters(",");
+                parser.CommentTokens = new[] { "#" };
+
+                var tt = new CreateRuntime()
+                {
+                    FileName = fileNameNoExt,
+                    Headers = parser.ReadFields(),
+                    Values = new List<string[]>(),
+                };
+
+                while (!parser.EndOfData)
+                {
+                    tt.Values.Add(parser.ReadFields());
+                }
+
+                var csContents = tt.TransformText();
+                File.WriteAllText(outputFilePath, csContents);
+            }
+        }
+    }
+}
---
+++ StateDisabilityWI_Example.cs
@@ -0,0 +1,161 @@
+﻿namespace AIM2023_CodeGeneration
+{
+    /// Example output from StateDisabilityWI.csv
+    public class StateDisabilityWI_Example
+    {
+        public string Code { get; set; }
+        public string Description { get; set; }
+        public string IsInvalid { get; set; }
+        public string ValidYearLow { get; set; }
+        public string ValidYearHigh { get; set; }
+        public string SkywardID { get; set; }
+
+        public static StateDisabilityWI_Example[] AllCodes = new StateDisabilityWI_Example[]
+        {
+            new StateDisabilityWI_Example()
+            {
+                Code = "!!",
+                Description = "Invalid",
+                IsInvalid = "True",
+                ValidYearLow = "0",
+                ValidYearHigh = "9999",
+                SkywardID = "C9384F45-AB4B-4AFA-B7DB-36871A59C4E9",
+            },
+            new StateDisabilityWI_Example()
+            {
+                Code = "A",
+                Description = "Autism",
+                IsInvalid = "False",
+                ValidYearLow = "0",
+                ValidYearHigh = "9999",
+                SkywardID = "80A8C1FF-7381-452D-B625-490D01393533",
+            },
+            new StateDisabilityWI_Example()
+            {
+                Code = "CD",
+                Description = "Cognitive Disability",
+                IsInvalid = "False",
+                ValidYearLow = "0",
+                ValidYearHigh = "2015",
+                SkywardID = "4C3AF1E7-11FD-49FF-A9B4-D7C07BC4E577",
+            },
+            new StateDisabilityWI_Example()
+            {
+                Code = "DB",
+                Description = "Deaf-Blind",
+                IsInvalid = "False",
+                ValidYearLow = "0",
+                ValidYearHigh = "9999",
+                SkywardID = "2A41B8D0-7266-4704-87E0-0007C6286999",
+            },
+            new StateDisabilityWI_Example()
+            {
+                Code = "EBD",
+                Description = "Emotional Behavioral Disability",
+                IsInvalid = "False",
+                ValidYearLow = "0",
+                ValidYearHigh = "9999",
+                SkywardID = "8FE70F16-BC07-454D-A824-998405FC2C02",
+            },
+            new StateDisabilityWI_Example()
+            {
+                Code = "H",
+                Description = "Hearing Impairment",
+                IsInvalid = "False",
+                ValidYearLow = "0",
+                ValidYearHigh = "2021",
+                SkywardID = "F44E9669-AEF1-4A08-A046-56A9774C03A7",
+            },
+            new StateDisabilityWI_Example()
+            {
+                Code = "H",
+                Description = "Deaf or Hard of Hearing",
+                IsInvalid = "False",
+                ValidYearLow = "2022",
+                ValidYearHigh = "2022",
+                SkywardID = "D937120F-50C6-431E-9CA2-F8A7660F451C",
+            },
+            new StateDisabilityWI_Example()
+            {
+                Code = "H",
+                Description = "Deaf and Hard of Hearing",
+                IsInvalid = "False",
+                ValidYearLow = "2023",
+                ValidYearHigh = "9999",
+                SkywardID = "88A02194-FF70-4EC8-B197-10CC8579A544",
+            },
+            new StateDisabilityWI_Example()
+            {
+                Code = "ID",
+                Description = "Intellectual Disability",
+                IsInvalid = "False",
+                ValidYearLow = "2016",
+                ValidYearHigh = "9999",
+                SkywardID = "C8126EA8-EA52-41B5-B122-DE86193E488C",
+            },
+            new StateDisabilityWI_Example()
+            {
+                Code = "LD",
+                Description = "Specific Learning Disability",
+                IsInvalid = "False",
+                ValidYearLow = "0",
+                ValidYearHigh = "9999",
+                SkywardID = "DF4A1D1C-7950-4BBC-84A2-A59018236F7B",
+            },
+            new StateDisabilityWI_Example()
+            {
+                Code = "OHI",
+                Description = "Other Health Impairment",
+                IsInvalid = "False",
+                ValidYearLow = "0",
+                ValidYearHigh = "9999",
+                SkywardID = "D4A9E85B-C43F-423B-A03D-3E622617B897",
+            },
+            new StateDisabilityWI_Example()
+            {
+                Code = "OI",
+                Description = "Orthopedic Impairment",
+                IsInvalid = "False",
+                ValidYearLow = "0",
+                ValidYearHigh = "9999",
+                SkywardID = "5C823606-138E-40C9-9553-48F86D6555EB",
+            },
+            new StateDisabilityWI_Example()
+            {
+                Code = "SDD",
+                Description = "Significant Developmental Delay",
+                IsInvalid = "False",
+                ValidYearLow = "0",
+                ValidYearHigh = "9999",
+                SkywardID = "CC8755B7-B9E4-4552-8A73-0CEBA96C86BD",
+            },
+            new StateDisabilityWI_Example()
+            {
+                Code = "SL",
+                Description = "Speech or Language Impairment",
+                IsInvalid = "False",
+                ValidYearLow = "0",
+                ValidYearHigh = "9999",
+                SkywardID = "DECAC17C-946E-4E5F-B511-39913DFE9666",
+            },
+            new StateDisabilityWI_Example()
+            {
+                Code = "TBI",
+                Description = "Traumatic Brain Injury",
+                IsInvalid = "False",
+                ValidYearLow = "0",
+                ValidYearHigh = "9999",
+                SkywardID = "D495805D-2B08-4AD9-9491-18CFF75185F0",
+            },
+            new StateDisabilityWI_Example()
+            {
+                Code = "V",
+                Description = "Visual Impairment",
+                IsInvalid = "False",
+                ValidYearLow = "0",
+                ValidYearHigh = "9999",
+                SkywardID = "261E1C95-5A87-4CB2-9BA9-3DDA5C5F57E5",
+            },
+        };
+    }
+}
